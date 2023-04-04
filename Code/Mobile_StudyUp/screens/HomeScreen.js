020b2ba04ebbf747787ebbdce0f4ba29e5de8492
CsNDavidEnd <103198171+CsNDavidEnd@users.noreply.github.com>
2023-04-04 17:47:28 +0200

    BasicHomeScreen

---
+++ Code/Mobile_StudyUp/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+      </View>
+    )
+  }
+
+  export default HomeScreen
+
+  const styles = StyleSheet.create({
+    
+  })
