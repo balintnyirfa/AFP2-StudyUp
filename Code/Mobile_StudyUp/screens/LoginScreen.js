d199408931342679c67281c323a4908b0b444b5f
CsNDavidEnd <103198171+CsNDavidEnd@users.noreply.github.com>
2023-04-04 17:48:48 +0200

    BasicLoginSreen

---
+++ Code/Mobile_StudyUp/screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>LoginScreen</Text>
+      </View>
+    )
+  }
+
+  export default LoginScreen
+
+  const styles = StyleSheet.create({
+    
+  })
