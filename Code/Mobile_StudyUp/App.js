a91b35f37f5958d56d00fc51589802d5374e4487
CsNDavidEnd <103198171+CsNDavidEnd@users.noreply.github.com>
2023-04-04 17:52:30 +0200

    AppJSImplementingScreens

--- Code/Mobile_StudyUp/App.js
+++ Code/Mobile_StudyUp/App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+      <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
   );
