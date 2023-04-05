77cfad91882d5986d716720c8d615dbb116a0916
CsNDavidEnd <103198171+CsNDavidEnd@users.noreply.github.com>
2023-04-05 06:43:55 +0200

    ErrorWix

--- Code/Mobile_StudyUp/screens/LoginScreen.js
+++ Code/Mobile_StudyUp/screens/LoginScreen.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('')
@@ -28,13 +28,13 @@ const LoginScreen = () => {
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity
-          onPress={handleLogin}
+          // onPress={handleLogin}
           style={styles.button}
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={handleSignUp}
+          // onPress={handleSignUp}
           style={[styles.button, styles.buttonOutline]}
         >
           <Text style={styles.buttonOutlineText}>Register</Text>
