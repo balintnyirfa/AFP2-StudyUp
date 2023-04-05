8a4fcf43450485b3be3ce4f6bee3e385c5e35f3e
CsNDavidEnd <103198171+CsNDavidEnd@users.noreply.github.com>
2023-04-05 06:31:00 +0200

    AlapLoginKinezet

--- Code/Mobile_StudyUp/screens/LoginScreen.js
+++ Code/Mobile_StudyUp/screens/LoginScreen.js
@@ -2,11 +2,46 @@ import React from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
     return (
-      <View style={styles.container}>
-        <Text>LoginScreen</Text>
+      <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
       </View>
-    )
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleLogin}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  )
   }
 
   export default LoginScreen
