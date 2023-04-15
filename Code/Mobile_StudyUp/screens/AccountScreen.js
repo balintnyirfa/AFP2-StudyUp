c72402370da3cc66dbed7a80410f768bfdbe9820
Lili <99401028+Lili0114@users.noreply.github.com>
2023-04-15 23:52:09 +0200

    Update AccountScreen.js

--- Code/Mobile_StudyUp/screens/AccountScreen.js
+++ Code/Mobile_StudyUp/screens/AccountScreen.js
@@ -1,61 +1,75 @@
 import React, { useState } from 'react';
-import { KeyboardAvoidingView, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View, Button } from 'react-native';
+import { KeyboardAvoidingView, Image, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View, Button, Platform} from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
 
 const AccountScreen = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [points, setPoints] = useState(0);
-  const [profilePic, setProfilePic] = useState('');
+  const [profilePic, setProfilePic] = useState(null);
   const [numberCurriculumDone, setNumberCurriculumDone] = useState(0);
 
   const handleSave = () => {
     console.log(`Username: ${username}, Email: ${email}, Points: ${points}, Profile Pic: ${profilePic}`);
   };
 
+  const handlePickImage = async () => {
+    // engedély galériához
+    if (Platform.OS !== 'web') {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Szükséges az engedély megadása, hogy képet feltölthess!');
+        return;
+      }
+    }
+  
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setProfilePic(result.uri);
+    }
+  };
+
   return (
     <KeyboardAvoidingView style={styles.container}>
     <View style={styles.inputContainer}>
-      <Text style={{ fontSize: 20, marginBottom: 70, color: '#8562AC', fontWeight: 500 }}>Felhasználói fiók</Text>
+      <Text style={{ fontSize: 20, marginBottom: 30, color: '#8562AC', fontWeight: 500 }}>Felhasználói fiók</Text>
       <View style={{ alignItems: 'center', marginBottom: 10 }}>
         <Image
-          style={{ width: 100, height: 100, borderRadius: 50 }}
+          style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 20 }}
           source={{ uri: profilePic }}
         />
         <View style={styles.ButtonContainer}>
-        <Pressable style={styles.loginButton}>
-            <Text style={styles.loginButtonText} onPress={() => alert("Kép feltöltése")}>Válassz profilképet!</Text>
+        <Pressable style={styles.uploadButton}>
+        {profilePic && <Image source={{ uri: profilePic }} />}
+        <Text style={styles.btnText}  onPress={handlePickImage}>
+              Profilkép feltöltése
+          </Text>
         </Pressable>
         </View>
       </View>
 
-      <TextInput
-        style={styles.inputTop}
-        onChangeText={setUsername}
-        value={username}
-        placeholder="Username"
-      />
-
-      <TextInput
-        style={styles.inputTop}
-        onChangeText={setEmail}
-        value={email}
-        placeholder="Email"
-        
-      />
-      
-      <Text style={styles.inputTop}>Pontok: {points}</Text>
-      <Text style={styles.inputTop} marginBottom={10} >Kitöltött tesztek száma: {points}</Text>
+      <Text style={styles.inputTop}><b>Felhasználónév:</b> -insert username-{username}</Text>
+      <Text style={styles.inputTop}><b>Email cím:</b> -insert email-{email}</Text>
+      <Text style={styles.inputTop}><b>Pontok:</b> {points}</Text>
+      <Text style={styles.inputTop} marginBottom={10} ><b>Kitöltött tesztek száma:</b> {points}</Text>
 
       <View style={styles.buttonContainer}>
-      <Pressable style={styles.loginButton} >
-        <Text style={styles.loginButtonText} onPress={handleSave}>Mentés</Text>
+      <Pressable style={styles.saveButton} >
+        <Text style={styles.btnText} onPress={handleSave}>Mentés</Text>
       </Pressable>
       </View>
 
     </View>
     </KeyboardAvoidingView>
   );
-};
+}
+;
 
 export default AccountScreen;
 
@@ -83,13 +97,13 @@ const styles = StyleSheet.create({
     },
   
     inputTop: {
-      borderWidth: 1,
-      borderColor: '#C9C9C9',
+      textAlign: 'center',
       borderTopLeftRadius: 7,
       borderTopRightRadius: 7,
       padding: 13,
       fontSize: 17,
-      color: '#BBBBBB'
+      color: '#5E5E5E',
+      
     },
   
     inputBottom: {
@@ -108,22 +122,40 @@ const styles = StyleSheet.create({
       marginLeft: 'auto',
       marginRight: 'auto'
     },
-  
-    loginButton: {
-        width: 350,
-        margin: 4,
-        padding: 13,
+
+    saveButton: {
+      width: 350,
+      marginBottom: 4,
+      marginTop: 4,
+      padding: 13,
+      backgroundColor: '#8562AC',
+      color: '#8562AC',
+      borderRadius: 7,
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 4,
+      elevation: 3,
+  },
+
+    uploadButton: {
+        width: 200,
         backgroundColor: '#8562AC',
-        borderRadius: 7
+        borderRadius: 7,
+        textAlign: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4
     },
   
-    loginButtonText: {
-        fontSize: 18,
-        color: '#FFFFFF',
+    btnText: {
+        fontSize: 16,
         marginLeft: 'auto',
-        marginRight: 'auto'
+        marginRight: 'auto',
+        color: '#FFFFFF'
     },
-  
+
     passwordForgotButton: {
       width: 350,
       margin: 4,
