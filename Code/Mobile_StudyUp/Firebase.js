5375305c130d393ed1c0013db38595edbedf2d7e
Nyirfa Bálint <balint.nyirfa@gmail.com>
2023-04-16 18:00:06 +0200

    Firebase download + database build start

--- Code/Mobile_StudyUp/Firebase.js
+++ Code/Mobile_StudyUp/Firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDATBpeUnKBRHxYo1yNCtwxtmPrwWyY3gk",
   authDomain: "studyup-258d0.firebaseapp.com",
@@ -16,14 +12,24 @@ const firebaseConfig = {
   measurementId: "G-2C8N82XE7E"
 };
 
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const reference = ref(db, 'users/' + userId);
+
+set(reference, {
+  username: username,
+  email: email,
+  password: password
+});
+
 // Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app()
-}
-
-const auth = firebase.auth()
-
-export { auth };
+//let app;
+//if (firebase.apps.length === 0) {
+//  app = firebase.initializeApp(firebaseConfig);
+//} else {
+//  app = firebase.app()
+//}
+//
+//const auth = firebase.auth()
+//
+//export { auth };
