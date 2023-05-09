fad11696766ad0da694fef362a78e08e40d1ab7f
Deli Daniella <113984678+Daniella1818@users.noreply.github.com>
2023-05-09 19:39:22 +0200

    Ranglista módosítása

--- Code/Mobile_StudyUp/screens/Leaderboard.js
+++ Code/Mobile_StudyUp/screens/Leaderboard.js
@@ -2,3 +2,54 @@ import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { Icon, ListItem } from 'react-native-elements';
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const [leaderboardData, setLeaderboardData] = useState([]);
+
+export default LeaderBoard
+
+const LeaderBoard = () => {
+    const [leaderboardData, setLeaderboardData] = useState([]);
+
+    useEffect(() => {
+        firebase.database().ref('leaderboard')
+          .orderByScore('score')
+          .limitToLast(10)
+          .on('value', (snapshot) => {
+            const data = [];
+            snapshot.forEach((childSnapshot) => {
+              const user = childSnapshot.val();
+              data.push({
+                id: childSnapshot.key,
+                name: user.name,
+                score: user.score,
+              });
+            });
+            setLeaderboardData(data.reverse());
+          });
+      }, []);
+
+    const renderItem = ({ item, index }) => (
+        <View style={styles.item}>
+          <View style={styles.itemRank}>
+            <Text>{index + 1}</Text>
+          </View>
+          <View style={styles.itemDetails}>
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemScore}>{item.score}</Text>
+          </View>
+          <View style={styles.itemIcon}>
+            <Icon name="trophy" size={24} color="#FFD700" />
+          </View>
+        </View>
+    );
+
+    return (
+        <View>
+            <FlatList
+                data={leaderboardData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+            />
+        </View>
+    );
+};
