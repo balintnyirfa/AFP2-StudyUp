fa16c3a605ad9f6fa0202e8ff3789886901d8852
Deli Daniella <113984678+Daniella1818@users.noreply.github.com>
2023-05-09 19:38:19 +0200

    Ranglista létrehozása

---
+++ Code/Mobile_StudyUp/screens/Leaderboard.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { Icon, ListItem } from 'react-native-elements';
+import { SafeAreaView } from "react-native-safe-area-context";
