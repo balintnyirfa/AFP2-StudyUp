e1ccd69ddeed6310a1444aa0a90c0ce1c502961a
Kiss Lili <lilcsi314@gmail.com>
2023-05-20 01:21:47 +0200

    kvízes cuccli mükszik juhé

---
+++ Code/Mobile_StudyUp/screens/QuizData.js
@@ -0,0 +1,29 @@
+var data;
+
+export default data = [
+    {
+        question: "Mikor kezdődött az első világháború?",
+        options: ["1914","1918","1920","1945"],
+        correct_option: "1914"
+    },
+    {
+        question: "Ki nem tartozott a Központi Hatalmakhoz?",
+        options: ["Németország","Oroszország","Osztrák-Magyar Monarchia"],
+        correct_option: "Oroszország"
+    },
+    {
+        question: "Ki nem tartozott az Antanthoz?",
+        options: ["Franciaország","Nagy-Britannia","Oroszország","Oszmán Birodalom"],
+        correct_option: "Oszmán Birodalom"
+    },
+    {
+        question: "Mikor volt a trianoni békeszerződés?",
+        options: ["1918","1920","1922","1930"],
+        correct_option: "1920"
+    },
+    {
+        question: "Melyik nagyhatalom győzött?",
+        options: ["Antant","Központi Hatalmak","nem volt győztes","nem tudom"],
+        correct_option: "Antant"
+    }
+]
